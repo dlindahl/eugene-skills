e6ac2359ef7a760781abf04b27d7edfa0051e61a
Derek Lindahl <derek.lindahl@gmail.com>
2016-12-11 19:36:40 -0800

    Update invocations

--- functions/watch-tv/model/intents.js
+++ functions/watch-tv/model/intents.js
@@ -1,9 +1,10 @@
+// Alexa, tell the TV to...
 module.exports = {
   TuneTvIntent: [
     '(change the channel to|tune in to|watch) {channel}',
     '(change the channel to|tune in to|watch) {channelNum}'
   ],
   WatchTvIntent: [
-    '(turn on the|watch) (TV|television)'
+    'turn (on|off)'
   ]
 }
